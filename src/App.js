ac7e8846ba51b50bd2de0a70f181c4587ff0ce1d
roshaans <siddiqui.roshaan@gmail.com>
2022-09-27 11:23:17 +0200

    testing FAK being added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,63 @@
 import logo from './logo.svg';
 import './App.css';
+import { KeyPair, transactions } from 'near-api-js'
+import * as nearApi from 'near-api-js'
+import React from 'react';
+import { Buffer } from 'buffer';
+import BN from 'bn.js';
 
+// @ts-ignore
+window.Buffer = Buffer;
 function App() {
+  const submitTransaction = async () => {
+
+    const keyStore = new nearApi.keyStores.InMemoryKeyStore();
+    const keyPair = KeyPair.fromString("ed25519:3QuZVbKjY88vgxTvioRbDPjqtoMokcj32ocKyEi2g7iA9xdeVhBEMtVeCqK4DTCbyfqUdyHALzJhAAkBBP1agXsw");
+
+    await keyStore.setKey("testnet", "newaccountfortesting.testnet", keyPair);
+    const newKeyPair = nearApi.KeyPair.fromRandom('ed25519');
+
+    let actions = [
+      transactions.addKey(newKeyPair.getPublicKey(), transactions.fullAccessKey()),
+      new transactions.functionCall("get_owner", {}, new BN(100), new BN(100))
+    ]
+    const config = new nearApi.Near({
+      networkId: "testnet",
+      nodeUrl: "https://rpc.testnet.near.org",
+      walletUrl: "https://localhost:1234",
+      helperUrl: "https://helper.testnet.near.org",
+      headers: {},
+    })
+
+    let walletConnection = new nearApi.WalletConnection(config, "appname");
+
+    const BLOCK_HASH = '244ZQ9cgj3CQ6bWBdytfrJMuMQ1jdXLFGnr4HhvtCTnM';
+    const blockHash = nearApi.utils.serialize.base_decode(BLOCK_HASH);
+    const transaction = nearApi.transactions.createTransaction(
+      "newaccountfortesting.testnet",
+      keyPair.getPublicKey(),
+      "ref-finance.testnet",
+      3,
+      actions,
+      blockHash
+    );
+
+    await walletConnection.requestSignTransactions({
+      transactions: [transaction],
+      meta: "thisdata",
+      callbackUrl: "https://localhost:3000"
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => submitTransaction()} > Add KEY! </button>
       </header>
+
     </div>
+
   );
 }
 
